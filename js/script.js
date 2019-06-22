c90a5c62a86c700b88d544027727b37aebcf72df
VladimirZyabkin <51987085+VladimirZyabkin@users.noreply.github.com>
2019-06-22 14:04:04 +0300

    Portfolio's carousel is done
    now carousel work as loop and active by hover

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     resizeImgSlider();
+    setPortfolio();
 }
 window.onresize = function(){
     resizeImgSlider();
@@ -13,7 +14,38 @@ function resizeImgSlider(){
         
     }
 }
-
+// -------- PORTFOLIO CAROUSEL ---------
+function getPortfolioData(){
+    var elem = document.querySelectorAll('#portfolio .portfolio_content_block');
+    var firstBlock = elem[0].getBoundingClientRect();
+    var nextBlock = elem[1].getBoundingClientRect();
+    var size = nextBlock.left - firstBlock.left;
+    var portfolio = document.getElementById('portfolio');
+    var portfolioMask = document.querySelector('.portfolio_content_mask');
+    return {
+        size: size,
+        portfolio: portfolio,
+        portfolioMask: portfolioMask
+    };
+}
+function setPortfolio(){
+    var data = getPortfolioData();
+    data.portfolio.style.width = data.size * 4 - 1 + "px";
+    data.portfolioMask.style.width = data.size * data.portfolioMask.children.length + 'px';
+    data.portfolioMask.style.left = -data.size + 'px';
+}
+function pressLeft(){
+    var data = getPortfolioData();
+    data.portfolioMask.removeChild(data.portfolioMask.children[data.portfolioMask.children.length - 1]);
+    var clonePMask = data.portfolioMask.cloneNode(true);
+    data.portfolioMask.insertBefore(clonePMask.children[clonePMask.children.length - 2], data.portfolioMask.childNodes[0]);
+}
+function pressRight(){
+    var data = getPortfolioData();
+    data.portfolioMask.removeChild(data.portfolioMask.children[0]);
+    var clonePMask = data.portfolioMask.cloneNode(true);
+    data.portfolioMask.appendChild(clonePMask.children[1]);
+}
 // -------- PORTFOLIO CAROUSEL ---------
 var portfolioIdx = 0;
 function selectPortfolio(course){
